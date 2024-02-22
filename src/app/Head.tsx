06ea5ecee7f2a2d55a27521e7ebf3cf175f5ec7d
clevessonmendonca <wanderbloks311@gmail.com>
2024-02-22 15:02:44 -0300

    feat: add google tags

--- src/app/Head.tsx
+++ src/app/Head.tsx
@@ -64,6 +64,11 @@ export const Head = () => {
       />
       <meta name="google-adsense-account" content="ca-pub-9125682874703477" />
       <meta name="robots" content="all" />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9125682874703477"
+        crossOrigin="anonymous"
+      ></script>
 
       <link rel="icon" href="favicon.png" sizes="any" />
       <title>Baratao Supermercados | O Herói dos Preços Baixos!</title>
